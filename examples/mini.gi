f0cde178285dc7f8c0dbb226231918e7795222d7
Max Horn <max@quendi.de>
2021-06-02 15:24:06 +0200

    tweak syntax highlighting

--- examples/mini.gi
+++ examples/mini.gi
@@ -10,11 +10,26 @@ for n in [1..100] do
   for i in [1..NrSmallGroups(n)] do
     G := SmallGroup(n,i);
     Print("SmallGroup(", n, ",", i, ") is abelian? ", IsAbelian(G), "\n");
+    if IsPerfectGroup(G) = true then
+      break;
+    fi;
   od;
 od;
+
+BindGlobal("foo", 1);
+BIND_GLOBAL("bar", 2);
 
 r := rec(
     a := [1,2,3],
     b := true,
     c := fail,
    );
+
+InstallImmediateMethod( IsFinitelyGeneratedGroup,
+    IsGroup and HasGeneratorsOfGroup,
+    function( G )
+    if IsFinite( GeneratorsOfGroup( G ) ) then
+      return true;
+    fi;
+    TryNextMethod();
+    end );
--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,4 +1,28 @@
 ; highlights.scm
+; based on https://github.com/tree-sitter/tree-sitter-python/blob/master/queries/highlights.scm
+
+; Builtin functions
+
+((call
+  function: (identifier) @function.builtin)
+ (#match?
+   @function.builtin
+   "^(Assert|Info|IsBound|Unbind|TryNextMethod)$"))
+
+; Function calls
+(call
+  function: (identifier) @function)
+
+; convention: identifiers that start with upper case are "global"
+;((identifier) @constructor
+; (#match? @constructor "^[A-Z]"))
+
+; convention: constants are of the form ALL_CAPS_AND_UNDERSCORES
+((identifier) @constant
+ (#match? @constant "^[A-Z][A-Z_]*$"))
+
+; any other identifier is a variable
+(identifier) @variable
 
 ; Literals
 
