ec8719c2ab01a2b6265238f26a6d450a0cd0c36d
reiniscirpons <rc234@st-andrews.ac.uk>
2024-08-23 02:56:13 +0100

    feat: Improve local queries

--- queries/locals.scm
+++ queries/locals.scm
@@ -1,21 +1,48 @@
-((lambda) @local.scope
- (#set! local.scope-inherits false))
-((function) @local.scope
- (#set! local.scope-inherits false))
+; The only other scopes apart from the global scope occur in function
+; definitions.
+[
+  (source_file)
+  (lambda)
+  (function)
+  (atomic_function)
+] @local.scope
 
+; TODO: (reiniscirpons) figure out if last match wins for local queries as well.
+; If not move this to end.
+(identifier) @local.reference
+
+(assignment_statement
+  left: (identifier) @local.definition.var)
+
+(locals
+  (identifier) @variable.definition.var)
+
+(for_statement
+  identifier: (identifier) @variable.definition.var)
+
+(assignment_statement
+  left: (identifier) @local.definition.function
+  right: [
+    (lambda)
+    (function)
+    (atomic_function)
+  ])
 
 (parameters
-  (identifier) @local.definition)
+  (identifier) @local.definition.parameter)
+
 (qualified_parameters
-  (identifier) @local.definition)
+  (identifier) @local.definition.parameter)
+
 (qualified_parameters
   (qualified_identifier
-    (identifier) @local.definition))
-(lambda_parameters
-  (identifier) @local.definition)
-(locals
-  (identifier) @local.definition)
+    (identifier) @local.definition.parameter))
 
-(identifier) @local.reference
-(qualified_identifier
-  (identifier) @local.reference)
+(lambda_parameters
+  (identifier) @local.definition.parameter)
+
+(record_entry
+  left: [
+    (identifier)
+    (integer)
+  ] @local.definition.field)
