1d8ec722106bca2318e306515ed2eaec4e35a66c
reiniscirpons <rc234@st-andrews.ac.uk>
2024-12-26 17:11:37 +0000

    fix: Fix go binding url for tree-sitter-gap

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 
 	tree_sitter "github.com/tree-sitter/go-tree-sitter"
-	tree_sitter_gap "github.com/gap-system/tree-sitter-gap/bindings/go"
+	tree_sitter_gap "github.com/gap-system/tree-sitter-gap"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
