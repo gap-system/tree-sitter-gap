0d0ebe628c062f6841c51f7ed1807208116a77d8
reiniscirpons <rc234@st-andrews.ac.uk>
2026-01-26 18:11:33 +0100

    chore: Update bindings

--- bindings/node/binding.cc
+++ bindings/node/binding.cc
@@ -9,7 +9,6 @@ const napi_type_tag LANGUAGE_TYPE_TAG = {0x8AF2E5212AD58ABF,
                                          0xD5006CAD83ABBA16};
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  exports["name"] = Napi::String::New(env, "gap");
   auto language = Napi::External<TSLanguage>::New(env, tree_sitter_gap());
   language.TypeTag(&LANGUAGE_TYPE_TAG);
   exports["language"] = language;
